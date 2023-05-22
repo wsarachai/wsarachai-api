215b42a49f849f8244510b8b742364c12121439f
Watcharin Sarachai <watcharin_s@mju.ac.th>
2023-05-22 23:14:44 +0700

    add course to line config

--- frontend/utils/lineConfig.js
+++ frontend/utils/lineConfig.js
@@ -1,11 +1,13 @@
 const line = require('@line/bot-sdk');
 
 const config241 = {
+  course: "IT241",
   channelAccessToken: process.env.LINE_ACCESS_TOKEN241,
   channelSecret: process.env.LINE_CHANNEL_SECRET241
 };
 
 const config493 = {
+  course: "IT493",
   channelAccessToken: process.env.LINE_ACCESS_TOKEN493,
   channelSecret: process.env.LINE_CHANNEL_SECRET493
 };
