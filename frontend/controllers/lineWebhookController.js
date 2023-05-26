a898bc0cd08fd44bb7a306a7098534158af032c0
Watcharin Sarachai <watcharin_s@mju.ac.th>
2023-05-26 13:13:12 +0700

    Work with LIFF app

--- frontend/controllers/lineWebhookController.js
+++ frontend/controllers/lineWebhookController.js
@@ -9,3 +9,9 @@ exports.webHook493 = (req, res) => {
     Promise.all(req.body.events.map(lineClient.handleEvent493))
         .then((result) => res.json(result));
 };
+
+exports.auth = (req, res) => {
+    console.log(req.query);
+    //res.status(200).send("hello");
+    res.redirect('/watcharin/test.html');
+};
